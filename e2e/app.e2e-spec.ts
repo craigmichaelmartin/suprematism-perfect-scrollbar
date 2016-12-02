craigmichaelmartin/suprematism-card-navbar

repository generated with base-component

e2e/app.e2e-spec.ts
@@ -1,0 +1,14 @@
+import { SuprematismCardNavbarPage } from './app.po';
+
+describe('suprematism-card-navbar App', function() {
+  let page: SuprematismCardNavbarPage;
+
+  beforeEach(() => {
+    page = new SuprematismCardNavbarPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});

e2e/app.po.ts
@@ -1,0 +1,12 @@
+import { browser, element, by } from 'protractor';
+
+export class SuprematismCardNavbarPage {
+
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('supre-root h1')).getText();
+  }
+}

craigmichaelmartin/suprematism-perfect-scrollbar

e2e/app.e2e-spec.ts
@@ -1,0 +1,14 @@
+import { SuprematismPerfectScrollbarPage } from './app.po';
+
+describe('suprematism-perfect-scrollbar App', function() {
+  let page: SuprematismPerfectScrollbarPage;
+
+  beforeEach(() => {
+    page = new SuprematismPerfectScrollbarPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});

e2e/app.po.ts
@@ -1,0 +1,12 @@
+import { browser, element, by } from 'protractor';
+
+export class SuprematismPerfectScrollbarPage {
+
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('supre-root h1')).getText();
+  }
+}